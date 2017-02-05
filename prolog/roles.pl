184382e68c0581accc2261e3f72c165a8fc2dec8
root <negenentwintig@hotmail.com>
2017-02-05 22:17:11 +0000

    improve action mods

--- prolog/roles.pl
+++ prolog/roles.pl
@@ -1,24 +1,16 @@
 role_action(R, A, T, C) :- role_action(R, A, T, C, [], _).
 role_action(R, A, T, C, O) :- role_action(R, A, T, C, [], O).
 
-role_action(([], Role), Action, [Target], Channel, ActionMods, ActionMods) :-
+role_action(([], Role), Action, Targets, Channel, ActionMods, ActionMods) :-
   main_role_action(Role, Action),
   (member(phase, ActionMods), !; default_phase_constraint(Role)),
-  (member(target, ActionMods), !; default_target_constraint(Role, Channel, Target)).
-
-% simple limiting modifiers such as 1-shot
-role_action(([Mod | Mods], Role), Action, Targets, Channel, LeftActionMods, RightActionMods) :-
-  role_action_filter(Mod, Action, Targets, Channel), !,
-  role_action(([Mods], Role), Action, Targets, Channel, LeftActionMods, RightActionMods).
+  (member(target, ActionMods), !; default_target_constraint(Role, Channel, Targets)).
 
 % special limiting modifiers, mostly ones that alter default behavior such as day and self
 role_action(([Mod | Mods], Role), Action, Targets, Channel, LeftActionMods, RightActionMods) :-
-  role_action_filter(Mod, Action, Targets, Channel, LeftActionMods, NewActionMods), !,
+  \+ role_action_filter(Mod, Action, Targets, Channel),
+  action_mods(Mod, LeftActionMods, NewActionMods),  
   role_action(([Mods], Role), Action, Targets, Channel, NewActionMods, RightActionMods).
-
-% special modifiers that apply when actions are processed, e.g. weak, processed in actions.pl
-role_action(([Mod | Mods], Role), Action, Targets, Channel, LeftActionMods, RightActionMods) :-
-  role_action(([Mods], Role), Action, Targets, Channel, [Mod | LeftActionMods], RightActionMods).
 
 default_phase_constraint(village) :-
   !, current_phase_name(day).
@@ -26,11 +18,14 @@ default_phase_constraint(village) :-
 default_phase_constraint(_) :-
   current_phase_name(night).
 
-default_target_constraint(_, Channel, Target) :-
-  (alive(Target); Target = noone),
-  (channel_type(Channel, global_role); other(Channel, Target)).
+alive_or_noone(Player) :- alive(Player).
+alive_or_noone(noone).
 
-other(Channel, Player) :- \+ access(Player, Channel).
+default_target_constraint(_, Channel, [Target]) :-
+  alive_or_noone(Target),
+  (channel_type(Channel, global_role); other(Target, Channel)).
+
+other(Player, Channel) :- \+ access(Player, Channel).
 
 main_role_action(village, lynch).
 main_role_action(cop, investigate).
@@ -42,10 +37,18 @@ role_action_filter(XShot, Action, _, Channel) :-
   append(X, "-shot", XShot),
   erl('Elixir.String':to_integer(X), N),
   count(action_history(_, action(_, Action, _, Channel), _), Count),
-  Count < N.
+  Count >= N.
 
-role_action_filter("self", _, Targets, Channel, Ci, [target | Ci]) :-
-  forall(member(Target, Targets), access(Target, Channel)).
+role_action_filter("self", _, Targets, Channel, Ci) :-
+  member(Target, Targets),
+  other(Target, Channel),
+  Target \= noone.
 
 role_action_filter("day", _, Targets, Channel, Ci, [target | Ci]) :-
   forall(member(Target, Targets), access(Target, Channel)).
+
+action_mod("self", N, [target | N]) :- !.
+action_mod("day", N, [phase | N]) :- !.
+
+% add any remaining role mods as an action mod; will be ignored if useless
+action_mod(Mod, N, [Mod | N]).
