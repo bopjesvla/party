c659a56aad2654c301a94dd498565cf81241439d
Bob <negenentwintig@hotmail.com>
2017-03-19 00:45:10 +0100

    upgrade

--- upgrade.exs
+++ upgrade.exs
@@ -1,9 +1,10 @@
-defmodule Mafia.DeployCallbacks do
+defmodule Mafia.UpgradeCallbacks do
   import Gatling.Bash
   def before_mix_digest(env) do
-    # mkdir prevents complains about this directory not existing
-    bash("mkdir", ~w[-p priv/static], cd: env.build_dir)
     bash("npm", ~w[install], cd: env.build_dir)
     bash("npm", ~w[run deploy], cd: env.build_dir)
+  end
+  def before_upgrade_service(env) do
+    bash("mix", ~w[ecto.migrate], cd: env.build_dir)
   end
 end
